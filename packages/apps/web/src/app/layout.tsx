ea34f7b1d72418c2cebe2dca27c33d3abcb7fa47
est-wiii <rs1kflvx@duck.com>
2024-08-13 04:03:08 +0900

    feat(web) add min-width to roote layout

--- packages/apps/web/src/app/layout.tsx
+++ packages/apps/web/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import { notoSansKr } from "~/components/fonts";
 import { ThemeProvider } from "~/components/theme-provider";
+import { cn } from "~/lib/utils";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={notoSansKr.className}>
+      <body className={cn(notoSansKr.className, "min-w-[400px]")}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
