e5f70aa59168042cc3a207278a94883d418496a7
est-wiii <rs1kflvx@duck.com>
2024-08-13 04:02:35 +0900

    feat(web) append components to home-page

--- packages/apps/web/src/app/page.tsx
+++ packages/apps/web/src/app/page.tsx
@@ -1,7 +1,19 @@
+import { Clock } from "~/features/clock";
+import { TodaysVerse } from "~/features/logos";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Wi space</h1>
+    <main className="flex flex-col justify-start items-center gap-y-4 p-24 min-h-screen">
+      <section className="w-full">
+        <div className="flex flex-row justify-between items-baseline gap-x-2 w-full">
+          <h1 className="flex font-bold text-left">Wi space</h1>
+          <Clock />
+        </div>
+      </section>
+
+      <section className="flex flex-col w-full">
+        <TodaysVerse />
+      </section>
     </main>
   );
 }
