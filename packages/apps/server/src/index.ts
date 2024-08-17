0ee381550a266553442fcd56d6eacf626bb388ae
est-wiii <rs1kflvx@duck.com>
2024-08-17 23:53:06 +0900

    chore(server) add server listen handler - log

--- packages/apps/server/src/index.ts
+++ packages/apps/server/src/index.ts
@@ -7,4 +7,6 @@ const PORT = import.meta.env.PORT ? Number(import.meta.env.PORT) : 8080;
 new Elysia()
   .use(userPlugin)
   .get('/', () => 'Hello from MySpace API')
-  .listen(PORT);
+  .listen(PORT, (server) => {
+    console.log('Server listening on', server.url.href);
+  });
